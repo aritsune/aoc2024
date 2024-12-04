e422d7ef9e9ba02d958b66391e1efeb00583b4a2
ari <sndaritsune@gmail.com>
2024-12-03 21:37:04 -0300

    day 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     println!("Day 1:");
     day1::solution();
     println!("Day 2:");
     day2::solution();
+    println!("Day 3:");
+    day3::solution();
 }
