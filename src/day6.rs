8f415466413c0db6e6035c87725245c4102b4d6d
ari <sndaritsune@gmail.com>
2024-12-07 19:50:24 -0300

    use position struct for day 6

--- src/day6.rs
+++ src/day6.rs
@@ -27,18 +27,52 @@ impl Direction {
     }
 }
 
+#[derive(Default)]
+struct MapPosition {
+    is_obstacle: bool,
+    already_visited_up: bool,
+    already_visited_right: bool,
+    already_visited_down: bool,
+    already_visited_left: bool,
+}
+
+impl MapPosition {
+    fn already_visited(&self) -> bool {
+        self.already_visited_up
+            || self.already_visited_down
+            || self.already_visited_left
+            || self.already_visited_right
+    }
+    fn already_visited_dir(&self, dir: &Direction) -> bool {
+        match dir {
+            Direction::Up => self.already_visited_up,
+            Direction::Right => self.already_visited_right,
+            Direction::Down => self.already_visited_down,
+            Direction::Left => self.already_visited_left,
+        }
+    }
+    fn set_already_visited(&mut self, dir: &Direction) {
+        match dir {
+            Direction::Up => self.already_visited_up = true,
+            Direction::Right => self.already_visited_right = true,
+            Direction::Down => self.already_visited_down = true,
+            Direction::Left => self.already_visited_left = true,
+        }
+    }
+}
+
 #[derive(Debug)]
-enum PatrolStep {
+enum PatrolSection {
     Stayed(i32, i32),
     Left(i32),
 }
 
 fn distance_to_obstacle(
-    map: &mut [Vec<char>],
+    map: &mut [Vec<MapPosition>],
     start_x: i32,
     start_y: i32,
     dir: &Direction,
-) -> PatrolStep {
+) -> PatrolSection {
     let mut distance = 0;
     let mut unrepeated_distance = 0;
     let mut cur_x = start_x;
@@ -56,23 +90,23 @@ fn distance_to_obstacle(
                 (Some(x), Some(y)) => (x, y),
                 // Otherwise, one of them is negative, we're out of bounds, so return Left from the
                 // main function
-                _ => return PatrolStep::Left(unrepeated_distance),
+                _ => return PatrolSection::Left(unrepeated_distance),
             }
         };
         // if this is Some then there is a character at (x, y)
-        if let Some(char) = map.get_mut(y).and_then(|row| row.get_mut(x)) {
-            if *char == '#' {
-                return PatrolStep::Stayed(distance, unrepeated_distance);
+        if let Some(pos) = map.get_mut(y).and_then(|row| row.get_mut(x)) {
+            if pos.is_obstacle {
+                return PatrolSection::Stayed(distance, unrepeated_distance);
             } else {
-                if *char != 'X' {
-                    *char = 'X';
+                if !pos.already_visited() {
+                    pos.set_already_visited(dir);
                     unrepeated_distance += 1;
                 }
                 distance += 1;
             }
         // otherwise, we went out of bounds with either x or y, so return Left
         } else {
-            return PatrolStep::Left(unrepeated_distance);
+            return PatrolSection::Left(unrepeated_distance);
         }
     }
 }
@@ -81,20 +115,26 @@ fn solve(raw_data: &str) {
     let mut cur_x: i32 = 0;
     let mut cur_y: i32 = 0;
     let mut cur_dir = Direction::Up;
-    let mut map: Vec<Vec<char>> = raw_data
+    let mut map: Vec<Vec<MapPosition>> = raw_data
         .lines()
         .enumerate()
         .map(|(y, l)| {
             l.chars()
                 .enumerate()
-                .map(|(x, l)| {
-                    if l == '^' {
-                        cur_x = x.try_into().expect("array index to always convert to i32");
-                        cur_y = y.try_into().expect("array index to always convert to i32");
-                        // Starting position is always an X
-                        return 'X';
-                    }
-                    l
+                .map(|(x, char)| {
+                    let mut out = MapPosition::default();
+                    match char {
+                        '^' => {
+                            cur_x = x.try_into().expect("array index to always convert to i32");
+                            cur_y = y.try_into().expect("array index to always convert to i32");
+                            // Starting position is always already visited
+                            out.already_visited_up = true;
+                        },
+                        '#' => out.is_obstacle = true,
+                        '.' => {},
+                        c => panic!("Unexpected character '{}'", c),
+                    } 
+                    out
                 })
                 .collect()
         })
@@ -103,7 +143,7 @@ fn solve(raw_data: &str) {
     let mut unique_distance = 1;
     loop {
         match distance_to_obstacle(&mut map, cur_x, cur_y, &cur_dir) {
-            PatrolStep::Stayed(total_dis, unique_dis) => {
+            PatrolSection::Stayed(total_dis, unique_dis) => {
                 unique_distance += unique_dis;
                 // Modify our x and y by adding our offset multiplied by the distance travelled
                 cur_x += cur_dir.to_offset().0 * total_dis;
@@ -111,7 +151,7 @@ fn solve(raw_data: &str) {
                 // Turn
                 cur_dir = cur_dir.turn_clockwise();
             }
-            PatrolStep::Left(unique_dis) => {
+            PatrolSection::Left(unique_dis) => {
                 unique_distance += unique_dis;
                 break;
             }
