fe1b55696928ed41e59a156ee41c97e3d1401987
ari <sndaritsune@gmail.com>
2024-12-07 17:36:11 -0300

    less stupid day 5

--- src/day5.rs
+++ src/day5.rs
@@ -22,9 +22,13 @@ pub fn fix_set(mut set: Vec<u8>, rules_map: &HashMap<u8, HashSet<u8>>) -> Vec<u8
         match rules_map.get(&set[i]) {
             None => continue,
             Some(not_allowed) => {
+                // Iterate over previous numbers
                 for j in 0..i {
+                    // If we run into any number that should be after set[i], swap them, then move
+                    // on to the next i
                     if not_allowed.contains(&set[j]) {
                         set[j..=i].rotate_right(1);
+                        break;
                     }
                 }
             }
@@ -62,11 +66,10 @@ pub fn solve(data: &str) {
     for updateline in data.lines().skip(split + 1) {
         if updateline.is_empty() { continue }
         let mut updateset: Vec<u8> = updateline.split(',').map(|strnum| strnum.parse::<u8>().expect("all numbers in input data to be parseable")).collect();
-        if set_is_valid(&updateset, &rules_map) { continue };
-        while !set_is_valid(&updateset, &rules_map) {
+        if !set_is_valid(&updateset, &rules_map) {
             updateset = fix_set(updateset, &rules_map);
+            fixed_sets.push(updateset);
         }
-        fixed_sets.push(updateset);
     }
     let answer_two: usize = fixed_sets.into_iter().map(|set| usize::from(set[set.len() / 2])).sum();
     println!("Total of middle numbers of fixed incorrect sets is {}", answer_two);
