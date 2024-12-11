c7e0da7fdd7936c19585fe4558396eb6aa865df0
ari <sndaritsune@gmail.com>
2024-12-10 21:23:14 -0300

    day 7 part 1

--- src/day7.rs
+++ src/day7.rs
@@ -1,21 +1,23 @@
+use std::fs;
+
 #[derive(Clone, Copy, PartialEq, Debug)]
 enum Operator {
     Plus,
     Times,
 }
 
-fn next_operator<T: PartialEq + Copy>(cur: T, list: &Vec<T>) -> Option<T> {
+fn next_operator<T: PartialEq + Copy>(cur: T, list: &[T]) -> Option<T> {
     let idx = list.iter().position(|i| *i == cur)? + 1;
     if idx > list.len() - 1 {
         return None;
     };
-    return Some(list[idx]);
+    Some(list[idx])
 }
 
 fn generate_operator_permutations(
     n: usize,
-    operators: Vec<Operator>,
-) -> impl Iterator<Item = Vec<Operator>> {
+    operators: &'_ [Operator],
+) -> impl Iterator<Item = Vec<Operator>> + '_ {
     let mut first_iter = true;
     let first_op = operators[0];
     let last_op = operators[operators.len() - 1];
@@ -41,9 +43,8 @@ fn generate_operator_permutations(
                 .rev()
                 .find(|(_, op)| **op != last_op)
                 .map(|(i, _)| i)?;
-            current[next_non_maxed_index] =
-                next_operator(dbg!(current[next_non_maxed_index]), &operators)
-                    .expect("next_op of next_non_maxed item to never be None");
+            current[next_non_maxed_index] = next_operator(current[next_non_maxed_index], operators)
+                .expect("next_op of next_non_maxed item to never be None");
             // Reset every item to the right of next_non_maxed_index to first operator
             current
                 .iter_mut()
@@ -54,7 +55,7 @@ fn generate_operator_permutations(
         } else {
             // Otherwise, just increment the operator at pos
             // Get the next operator from the list
-            current[last_pos] = next_operator(current[last_pos], &operators)
+            current[last_pos] = next_operator(current[last_pos], operators)
                 .expect("current[pos] to never be last_op");
             // Return our current state
             Some(current.clone())
@@ -62,11 +63,66 @@ fn generate_operator_permutations(
     })
 }
 
-pub fn solve(raw_data: &str) {}
+struct Equation {
+    test_value: i64,
+    numbers: Vec<i64>,
+}
+
+fn parse_raw_data(raw_data: &'_ str) -> impl Iterator<Item = Equation> + '_ {
+    raw_data
+        .lines()
+        .filter(|l| !l.is_empty())
+        .filter_map(|l| l.split_once(':'))
+        .map(|(test_val_string, numbers_string)| Equation {
+            test_value: str::parse(test_val_string).expect("test_val_string to be a valid i64"),
+            numbers: numbers_string
+                .trim()
+                .split(' ')
+                .map(|num_str| str::parse(num_str).expect("all numbers after : to be valid"))
+                .collect::<Vec<_>>(),
+        })
+}
+
+fn test_equation(equation: &Equation, operators: &[Operator]) -> bool {
+    if equation.numbers.is_empty() {
+        return false;
+    }
+    let mut op_permutations = generate_operator_permutations(equation.numbers.len() - 1, operators);
+    // Iterate over every permutation of len - 1 operators
+    // return true as soon as we find any match
+    op_permutations.any(|perm| {
+        equation
+            // Iterate over the numbers in the equation
+            .numbers
+            .iter()
+            // Skip the first number, as we'll use it as the init value when folding
+            .skip(1)
+            // Zip (combine) the number iterator with an iterator over the operators of this permutation
+            // Since we skipped the first value of `numbers`, these iterators should both be the same length
+            // (since perm.len() == numbers.len() - 1)
+            .zip(perm)
+            // Fold (reduce) by starting with the first number and then doing `acc = acc [operator] next` until we're out of iterations
+            .fold(equation.numbers[0], |acc, (n, operator)| match operator {
+                Operator::Plus => acc + n,
+                Operator::Times => acc * n,
+            })
+            // Compare the result of our fold with our test value
+            == equation.test_value
+    })
+}
+
+pub fn solve(raw_data: &str) {
+    let operators = vec![Operator::Plus, Operator::Times];
+    let total_sum: i64 = parse_raw_data(raw_data)
+        .filter_map(|eq| match test_equation(&eq, &operators) {
+            true => Some(eq.test_value),
+            false => None,
+        })
+        .sum();
+    println!("Total sum of valid equations is {}", total_sum);
+}
 
 pub fn solution() {
-    let res = generate_operator_permutations(4, vec![Operator::Plus, Operator::Times])
-        .collect::<Vec<_>>();
-    println!("{:#?}", res);
-    println!("{} items", res.len());
+    let raw_data = fs::read_to_string("input/day7input.txt").expect("Failed to read input file!");
+    solve(&raw_data);
 }
