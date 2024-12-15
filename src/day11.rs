f0e2833b5720f7f41453cca12ea9bfcbf19fa53a
ari <sndaritsune@gmail.com>
2024-12-15 15:14:44 -0300

    day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,10 +1,18 @@
 use crate::util::count_digits;
-use std::fs;
+use std::{collections::HashMap, fs, hash::Hash};
 
-pub fn get_blink_splits(mut num: usize, blinks: usize) -> usize {
+pub fn get_blink_splits(
+    orig_num: usize,
+    blinks: usize,
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if let Some(r) = cache.get(&(orig_num, blinks)) {
+        return *r;
+    }
     if blinks == 0 {
         return 0;
     }
+    let mut num = orig_num;
     let mut splits = 0;
     for i in 1..=blinks {
         let digits = count_digits(&num);
@@ -14,19 +22,21 @@ pub fn get_blink_splits(mut num: usize, blinks: usize) -> usize {
             splits += 1;
             let left = num / 10_usize.pow((digits / 2).try_into().unwrap());
             let right = num - left * 10_usize.pow((digits / 2).try_into().unwrap());
-            let left_splits = get_blink_splits(left, blinks - i);
+            let left_splits = get_blink_splits(left, blinks - i, cache);
             splits += left_splits;
-            let right_splits = get_blink_splits(right, blinks - i);
+            let right_splits = get_blink_splits(right, blinks - i, cache);
             splits += right_splits;
             break;
         } else {
             num *= 2024;
         }
     }
+    cache.insert((orig_num, blinks), splits);
     splits
 }
 
 pub fn solve(raw_data: &str) {
+    let mut cache = HashMap::<(usize, usize), usize>::new();
     let stones: Vec<usize> = raw_data
         .trim()
         .split(' ')
@@ -35,7 +45,7 @@ pub fn solve(raw_data: &str) {
     const BLINK_COUNT: usize = 25;
     let part1len: usize = stones
         .iter()
-        .map(|num| 1 + get_blink_splits(*num, BLINK_COUNT))
+        .map(|num| 1 + get_blink_splits(*num, BLINK_COUNT, &mut cache))
         .sum();
     println!(
         "Blinking {} times results in {} stones",
@@ -44,7 +54,7 @@ pub fn solve(raw_data: &str) {
     const BLINK_COUNT_PART2: usize = 75;
     let part2len: usize = stones
         .iter()
-        .map(|num| 1 + get_blink_splits(*num, BLINK_COUNT_PART2))
+        .map(|num| 1 + get_blink_splits(*num, BLINK_COUNT_PART2, &mut cache))
         .sum();
     println!(
         "Blinking {} times results in {} stones",
